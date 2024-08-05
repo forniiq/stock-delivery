0a89680264f3aaddf8e00d7ccbac1fc826e9dd2d
fixzoro <samwinchester@ya.md>
2024-08-05 18:27:31 +0300

    RIP

--- Player/player_1/Player_1.gd
+++ Player/player_1/Player_1.gd
@@ -1,85 +1,71 @@
 extends CharacterBody2D
 
-#Состояния
-enum {
-	MOVE, #Состояние передвижения
-	ON_VEHICLE, #Состояние если персонаж находится в транспорте
-	DIED #Состояние если персонаж столкнется с препятствиями (Враждебное существо, враждебный объект и т.п)
+# Состояния
+enum State {
+	MOVE,       # Состояние передвижения
+	ON_VEHICLE, # Состояние если персонаж находится в транспорте
+	DIED        # Состояние если персонаж столкнется с препятствиями
 }
 
-#Подключение узлов персонажа
+# Подключение узлов персонажа
 @onready var animPlayer = $AnimationPlayer
 @onready var anim = $AnimatedSprite2D
 
-#Обьявление переменных
-var state = MOVE #Состояние
-var speed = 150.0 #Скорость
-var run_speed = 1.0 #Множитель бега
-var controlling = true #Статус управления
-var on_area = false #Нахождение в области
-var move_vector #Сторона движения
+# Объявление переменных
+var state = State.MOVE   # Текущее состояние
+var speed = 150.0        # Скорость
+var run_speed = 1.0      # Множитель бега
+var controlling = true   # Статус управления
+var move_vector = ""     # Сторона движения
 
+# Обработчик готовности узла
 func _ready():
 	pass
-	
+
+# Обработчик физического процесса
 func _physics_process(_delta):
 	match state:
-		MOVE:
+		State.MOVE:
 			move_state()
-			
+
+	# Перемещение и скольжение
 	move_and_slide()
-			
-#Функция передвижения
+
+# Функция передвижения
 func move_state():
 	if controlling:
 		var direction = Input.get_vector("left", "right", "up", "down")
-		if direction:
+		if direction != Vector2.ZERO:
 			velocity = direction * speed * run_speed
-			if velocity.y == 0:
+			if direction.y == 0:
 				move_vector = "right"
-				if run_speed == 1:
-					animPlayer.play("walk")
-				else:
-					animPlayer.play("run")
-					
-			elif velocity.y < 1:
+				animPlayer.play("run" if run_speed > 1 else "walk")
+			elif direction.y < 0:
 				move_vector = "up"
-				if run_speed == 1:
-					animPlayer.play("walk_up")
-				else:
-					animPlayer.play("run_up")
-					
-			elif velocity.y > 1:
+				animPlayer.play("run_up" if run_speed > 1 else "walk_up")
+			elif direction.y > 0:
 				move_vector = "down"
-				if run_speed == 1:
-					animPlayer.play("walk_down")
-				else:
-					animPlayer.play("run_down")
-					
+				animPlayer.play("run_down" if run_speed > 1 else "walk_down")
 		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			velocity.y = move_toward(velocity.y, 0, speed)
+			# Плавное замедление
+			velocity = velocity.lerp(Vector2.ZERO, 0.1)
 			if move_vector == "right":
 				animPlayer.play("idle")
-			if move_vector == "up":
+			elif move_vector == "up":
 				animPlayer.play("idle_up")
-			if move_vector == "down":
+			elif move_vector == "down":
 				animPlayer.play("idle_down")
-					
-		if direction.x == -1:
-				anim.flip_h = true
-		elif direction.x == 1:
-				anim.flip_h = false
-				
-		if Input.is_action_pressed("sprint"):
-			run_speed = 1.5
-		else:
-			run_speed = 1.0
 
-#Функция нахождения в машине
+		# Определение направления взгляда персонажа
+		anim.flip_h = direction.x < 0
+
+		# Проверка на ускорение
+		run_speed = 1.5 if Input.is_action_pressed("sprint") else 1.0
+
+# Функция нахождения в машине
 func on_vehicle_state():
 	controlling = false
 
-#Функция смерти
+# Функция смерти
 func died_state():
 	controlling = false
